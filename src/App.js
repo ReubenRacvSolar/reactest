6402fdbbabff209c76f35646d7edbd04956fc45d
Reuben <Reuben.Fevreau@racvsolar.com.au>
2022-05-25 11:00:30 +1000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,171 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link,
+    useNavigate,
+    useLocation,
+    useParams
+  } from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <ModalSwitch />
+    </Router>
+  )
 }
 
-export default App;
+
+function ModalSwitch() {
+    let location = useLocation() 
+
+
+    let background = location.state && location.state.background
+
+    return (
+        <div>
+            <Routes>
+                <Route exact path = '/' element={<Home/>} />
+                <Route path="/gallery" element={<Gallery />} />
+        <Route path="/img/:id" element={<ImageView />} />
+            </Routes>
+
+            {background && <Route path="/img/:id" element={<Modal />} />}
+        </div>
+    )
+}
+
+const IMAGES = [
+    { id: 0, title: "Dark Orchid", color: "DarkOrchid" },
+    { id: 1, title: "Lime Green", color: "LimeGreen" },
+    { id: 2, title: "Tomato", color: "Tomato" },
+    { id: 3, title: "Seven Ate Nine", color: "#789" },
+    { id: 4, title: "Crimson", color: "Crimson" }
+  ];
+
+  function Thumbnail({ color }) {
+    return (
+      <div
+        style={{
+          width: 50,
+          height: 50,
+          background: color
+        }}
+      />
+    );
+  }
+  
+  function Image({ color }) {
+    return (
+      <div
+        style={{
+          width: "100%",
+          height: 400,
+          background: color
+        }}
+      />
+    );
+  }
+
+  function Home() {
+    return (
+      <div>
+        <Link to="/gallery">Visit the Gallery</Link>
+        <h2>Featured Images</h2>
+        <ul>
+          <li>
+            <Link to="/img/2">Tomato</Link>
+          </li>
+          <li>
+            <Link to="/img/4">Crimson</Link>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+  
+  function Gallery() {
+    let location = useLocation();
+  
+    return (
+      <div>
+        {IMAGES.map(i => (
+          <Link
+            key={i.id}
+            to={{
+              pathname: `/img/${i.id}`,
+              // This is the trick! This link sets
+              // the `background` in location state.
+              state: { background: location }
+            }}
+          >
+            <Thumbnail color={i.color} />
+            <p>{i.title}</p>
+          </Link>
+        ))}
+      </div>
+    );
+  }
+  
+  function ImageView() {
+    let { id } = useParams();
+    let image = IMAGES[parseInt(id, 10)];
+  
+    if (!image) return <div>Image not found</div>;
+  
+    return (
+      <div>
+        <h1>{image.title}</h1>
+        <Image color={image.color} />
+      </div>
+    );
+  }
+  
+  function Modal() {
+    let history = useNavigate();
+    let { id } = useParams();
+    let image = IMAGES[parseInt(id, 10)];
+  
+    if (!image) return null;
+  
+    let back = e => {
+      e.stopPropagation();
+      history.goBack();
+    };
+  
+    return (
+      <div
+        onClick={back}
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0,
+          background: "rgba(0, 0, 0, 0.15)"
+        }}
+      >
+        <div
+          className="modal"
+          style={{
+            position: "absolute",
+            background: "#fff",
+            top: 25,
+            left: "10%",
+            right: "10%",
+            padding: 15,
+            border: "2px solid #444"
+          }}
+        >
+          <h1>{image.title}</h1>
+          <Image color={image.color} />
+          <button type="button" onClick={back}>
+            Close
+          </button>
+        </div>
+      </div>
+    );
+  }
+  
